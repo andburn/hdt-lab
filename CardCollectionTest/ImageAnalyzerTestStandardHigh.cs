43f091b5360777bda6bd6968dce75aa381cd1a15
Andrew Burnett <andrew@nospam.forme>
2015-04-11 15:25:50 +0100

    Add image tests for multiple resolutions

---
+++ CardCollectionTest/ImageAnalyzerTestStandardHigh.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndBurn.HDT.Plugins.CardCollection
+{
+    /*
+     * Image tests at 1440x900
+     */
+    [TestClass]
+    public class ImageAnalyzerTestStandardHigh
+    {
+        private const string Dir = @"Files\1440x900\";
+
+        [TestMethod]
+        public void TestImage_NoMatches()
+        {
+            Assert.AreEqual(new CardCount(),
+                ImageAnalyzer.Recognize(Dir + "no_matches.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_1_0()
+        {
+            Assert.AreEqual(new CardCount(1, 0),
+                ImageAnalyzer.Recognize(Dir + "standard_1_0.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_2_0()
+        {
+            Assert.AreEqual(new CardCount(2, 0),
+                ImageAnalyzer.Recognize(Dir + "standard_2_0.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_2_1()
+        {
+            Assert.AreEqual(new CardCount(2, 1),
+                ImageAnalyzer.Recognize(Dir + "standard_2_1.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_2_2()
+        {
+            Assert.AreEqual(new CardCount(2, 2),
+                ImageAnalyzer.Recognize(Dir + "standard_2_2.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Legend_1_0()
+        {
+            Assert.AreEqual(new CardCount(1, 0),
+                ImageAnalyzer.Recognize(Dir + "legend_1_0.png", true));
+        }
+
+        [TestMethod]
+        public void TestImage_Legend_0_1()
+        {
+            Assert.AreEqual(new CardCount(0, 1),
+                ImageAnalyzer.Recognize(Dir + "legend_0_1.png", true));
+        }
+    }
+}
---
+++ CardCollectionTest/ImageAnalyzerTestWideHigh.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndBurn.HDT.Plugins.CardCollection
+{
+    /*
+     * Image tests at 1920x1080
+     */
+    [TestClass]
+    public class ImageAnalyzerTestWideHigh
+    {
+        private const string Dir = @"Files\1920x1080\";
+
+        [TestMethod]
+        public void TestImage_NoMatches()
+        {
+            Assert.AreEqual(new CardCount(),
+                ImageAnalyzer.Recognize(Dir + "no_matches.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_1_0()
+        {
+            Assert.AreEqual(new CardCount(1, 0),
+                ImageAnalyzer.Recognize(Dir + "standard_1_0.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_2_0()
+        {
+            Assert.AreEqual(new CardCount(2, 0),
+                ImageAnalyzer.Recognize(Dir + "standard_2_0.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_1_1()
+        {
+            Assert.AreEqual(new CardCount(1, 1),
+                ImageAnalyzer.Recognize(Dir + "standard_1_1.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_2_1()
+        {
+            Assert.AreEqual(new CardCount(2, 1),
+                ImageAnalyzer.Recognize(Dir + "standard_2_1.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_0_1()
+        {
+            Assert.AreEqual(new CardCount(0, 1),
+                ImageAnalyzer.Recognize(Dir + "standard_0_1.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Standard_2_2()
+        {
+            Assert.AreEqual(new CardCount(2, 2),
+                ImageAnalyzer.Recognize(Dir + "standard_2_2.png"));
+        }
+
+        [TestMethod]
+        public void TestImage_Legend_1_0()
+        {
+            Assert.AreEqual(new CardCount(1, 0),
+                ImageAnalyzer.Recognize(Dir + "legend_1_0.png", true));
+        }
+
+        [TestMethod]
+        public void TestImage_Legend_0_1()
+        {
+            Assert.AreEqual(new CardCount(0, 1),
+                ImageAnalyzer.Recognize(Dir + "legend_0_1.png", true));
+        }
+        
+    }
+}
