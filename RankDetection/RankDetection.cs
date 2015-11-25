880ea77d97b09f1d2de6af539cf3df2662f9508a
Andrew Burnett <andrew@nospam.forme>
2015-11-25 10:34:18 +0100

    Initial commit

---
+++ RankDetection/RankDetection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearthstone
+{
+    public class RankDetection
+    {
+        public Result Process()
+        {
+            return new Result();
+        }
+
+        public class Result
+        {
+            public int Player { get; private set; }
+            public int Opponent { get; private set; }
+            public bool Success { get; private set; }
+        }
+    }
+}
---
+++ RankDetection/TemplateCreator.cs
@@ -0,0 +1,11 @@
+﻿namespace Hearthstone
+{
+    public static class RankTemplate
+    {
+        // create the template images from the full textures
+        public static void Create(string dir)
+        {
+            
+        }
+    }
+}
