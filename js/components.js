5209d4ac8b9de4d79b6b3d798a03a1c6cdda44a8
Andrew Burnett <andrew@nospam.forme>
2016-01-07 16:13:26 +0000

    Add custom obj loader for multi materials

--- js/components.js
+++ js/components.js
@@ -1,94 +1,102 @@
 var components = (function(){
 
-  var addMesh = function(scene, texName, meshName, x, y, z, scale, offX, offY) {
-    var texture = new THREE.Texture();
-    var loader = new THREE.ImageLoader();
-    loader.load( 'textures/' + texName, function ( image ) {
-      texture.image = image;
-      if (offX != null) {
-        texture.offset.x = offX;
-        texture.offset.y = offY;
-      }
-      texture.needsUpdate = true;
-    } );
+  var loadMesh = function(obj, textures, meshName, x, y, z, scale) {
+    var loader, mat, materials = [];
 
-    loader = new THREE.OBJLoader();
+    mat = new THREE.MultiMaterial(materials);
+    loader = new THREE.ImageLoader();
+    for (var i = 0; i < textures.length; i++) {
+      loader.load( 'textures/' + textures[i], function ( image ) {
+        var texture = new THREE.Texture();
+        texture.image = image;
+        texture.needsUpdate = true;
+        materials.push(new THREE.MeshBasicMaterial( { map: texture }));
+        //mat.needsUpdate = true;
+      } );
+    }
+
+    loader = new THREE.OBJLoader2();
     loader.load( 'meshes/' + meshName, function ( object ) {
       object.traverse( function ( child ) {
         if ( child instanceof THREE.Mesh ) {
-          child.material = new THREE.MeshBasicMaterial( { map: texture } );
+          child.material = mat;
+          child.material.needsUpdate = true;
         }
       } );
       object.scale.set(scale, scale, scale);
       object.position.set(x, y, z);
-      scene.add( object );
+      obj.add(object);
     } );
   }
 
-  var base = function(scene) {
-    addMesh( scene,
-      'Card_Inhand_Generic.png',
+  var base = function(obj) {
+    loadMesh(obj,
+      ['Card_Back_Default_D.png', 'Card_Inhand_Generic.png'],
       'Card_InHand_Ally_Base_mesh.obj',
       0, 0, 0, 1, null, null
     );
   };
 
-  var portrait = function(scene) {
-    addMesh( scene,
-      'Card_Inhand_Generic.png',
+  var portrait = function(obj) {
+    loadMesh(obj,
+      ['GVG_119.png', 'Card_Inhand_Generic.png'],
       'Card_InHand_Ally_Portrait_mesh.obj',
       -0.0115, 0.0677, -0.6590, 1, null, null
     );
   };
 
-  var namebanner = function(scene) {
-    addMesh( scene,
-      'Card_InHand_BannerAtlas.png',
+  var namebanner = function(obj) {
+    loadMesh(obj,
+      ['Card_InHand_BannerAtlas2.png'],
       'AllyInHand_NameBanner_mesh.obj',
       -0.0016, 0.143, 0.0877, 1, null, null
     );
   }
 
-  var description = function(scene) {
-    addMesh( scene,
-      'Card_InHand_BannerAtlas.png',
+  var description = function(obj) {
+    loadMesh(obj,
+      ['Card_InHand_BannerAtlas2.png'],
       'Card_InHand_Ally_Description_mesh.obj',
       -0.02314, 0.0528, 0.7809, 1, null, null
     );
   }
 
-  var dragon = function(scene) {
-    addMesh( scene,
-      'Card_InHand_BannerAtlas.png',
+  var dragon = function(obj) {
+    loadMesh(obj,
+      ['Card_InHand_BannerAtlas2.png'],
       'Card_InHand_Ally_EliteDragon_mesh.obj',
       -0.01148, 0.0676, -0.6589, 1, null, null
     );
   }
 
-  var raceplate = function(scene) {
-    addMesh( scene,
-      'Card_InHand_BannerAtlas.png',
+  var raceplate = function(obj) {
+    loadMesh(obj,
+      ['Card_InHand_BannerAtlas2.png'],
       'AllyInHand_RacePlate_mesh.obj',
       -0.07, 0.056, 1.19, 1, null, null
     );
   }
 
-  var rarityFrame = function(scene) {
-    addMesh(scene,
-      'Card_Inhand_Generic.png',
+  var rarityFrame = function() {
+    createMesh(
+      ['Card_Inhand_Generic.png'],
       'Card_InHand_Ally_RarityGemFrame_mesh.obj',
       -0.0115, 0.0677, -0.6590, 1, -0.436, 0.132
     );
   }
 
+  var load = function(obj) {
+    portrait(obj);
+    base(obj);
+    namebanner(obj);
+    description(obj);
+    dragon(obj);
+    raceplate(obj);
+    // obj.add(rarityFrame());
+  }
+
   return {
-    base: base,
-    portrait: portrait,
-    namebanner: namebanner,
-    description: description,
-    rarityFrame: rarityFrame,
-    dragon: dragon,
-    raceplate: raceplate
+    load: load
   };
 
 })();
